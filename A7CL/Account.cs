05f5033057fe3f4112d2f74bd5be9f5870cc4e39
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-18 08:24:21 +0100

    Very small change in comment

--- A7CL/Account.cs
+++ A7CL/Account.cs
@@ -14,10 +14,7 @@ namespace A7CL
 
             https://social.msdn.microsoft.com/Forums/en-US/b89bd560-340a-4ab3-8ec9-c80e3120bf1f/code-contracts-cc1038-bug?forum=codecontracts
 
-            You cant use private fields as base for a property like this:
-
-            private double _balance=0;
-            public double Balance{get{return _balance;}}
+            You cant use private fields in Contract.<function>...
         */
 
         #region Properties
